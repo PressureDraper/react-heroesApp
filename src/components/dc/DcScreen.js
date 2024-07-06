fe6757a0bb01d80719da7ddc1bc1b2a90a0fb4ce
PressureDraper <warmarines00@hotmail.com>
2024-07-05 20:56:42 -0600

    add: dark mode v3

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { HeroList } from '../heroes/HeroList'
+import { UiContext } from '../../auth/UiContext';
 
 export const DcScreen = () => {
+  const { theme } = useContext(UiContext);
+
   return (
     <div>
-      <h1>DC Screen</h1>
-      <hr />
+      <h1 style={{ color: theme.theme === 'light' ? 'black' : 'whitesmoke' }}>DC Screen</h1>
+      <hr style={{ backgroundColor: 'white', height: '2px' }} />
       <HeroList publisher={'DC Comics'} />
     </div>
   )
